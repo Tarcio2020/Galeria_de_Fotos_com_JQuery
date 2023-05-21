2c6e28ca73e74862cdba55d4a9139d00f1376092
Tarcio Teles <tarcioapps7@gmail.com>
2023-05-21 19:21:56 -0300

    add_ Fase2

--- aula_1.js
+++ aula_1.js
@@ -5,10 +5,15 @@ $(document).ready(function() {/*{
     document.querySelector('header button').addEventListener('click', function(){
     })*/
         
-    $('header button').click(function(e){
-        alert("Expandir Foemulário")
+    $('header button').click(function() {
+        $('form').slideDown();
+        
+        //alert("Expandir Foemulário") 
     })
 
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
 
     $('form').on('submit', function(e){
         console.log("submit");
